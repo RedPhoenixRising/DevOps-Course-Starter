82c5fcd6aa0bc63d28a3ec16c0b1415e70d93bca
Steven Bennison <sdbennison@me.com>
2023-07-25 18:30:33 +0100

    Added functionality to add a new task

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 
 from todo_app.flask_config import Config
-from todo_app.data.session_items import get_items
+from todo_app.data.session_items import get_items,add_item
 app = Flask(__name__)
 app.config.from_object(Config())
 
@@ -10,3 +10,9 @@ app.config.from_object(Config())
 def index():
     items = get_items()
     return render_template('index.html', items=items)
+
+@app.route('/add', methods=["POST"])
+def post_item():
+    title = request.form["title"]
+    add_item(title)
+    return redirect("http://127.0.0.1:5000",302)
